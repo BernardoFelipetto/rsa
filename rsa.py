84946f9e4d14439e594fcc4b19f4ea8efd55d398
Bernardo Felipetto <bernardo.felipetto@hp.com>
2019-06-27 10:37:48 -0300

    final program

--- rsa.py
+++ rsa.py
@@ -3,149 +3,123 @@ import sys
 import hashlib
 import math
 import random
-from decimal import Decimal
+import binascii
 
-count = 0
 
+###
+# Para rodar apenas digite py -3 rsa.py no terminal
+###
 
 def generate_large_prime():
     is_prime = False
     while not is_prime:
+        #gera um numero aleatório de 1024 bits
         big_number = random.getrandbits(1024)
         is_prime = fermat(big_number)
     return big_number
 
 def fermat(x):
+    #verifica se o numero é primo usando o teorema de fermat
     return pow(2, x-1, x) == 1
 
-def calculate_cypher_key(relative_prime_number):
-    #escolher um numero aleatorio maior q 1 e menor q relative_prime_number
-    #calcular o computeGCD dele
-    #se for igual a 1 retorna o numero calculado
+def calculate_cypher_key(phi_number):
     sys.setrecursionlimit(1000000)
 
     is_cypher = 0
     while is_cypher != 1:
-        cypher_key = random.randrange(pow(2,1000), relative_prime_number)
-        is_cypher, a, b = computeGCD(cypher_key, relative_prime_number)
+        #Escolhe um numero aleatório entre 2^1000 representando os 1000 bits e phi
+        cypher_key = random.randrange(pow(2,1000), phi_number)
+        is_cypher, a, b = egcd(cypher_key, phi_number)
+        #Verifica se o seu GCD é 1 de acordo com o algoritmo fornecido
         if(is_cypher == 1):
             return cypher_key
 
-
-    #for x in range(pow(2, 1000), relative_prime_number):
-    #    is_cypher, a, b = computeGCD(x, relative_prime_number)
-    #    if(is_cypher == 1):
-    #        return x
-
-    #while not is_cypher:
-    #    cypher_key = random.randrange(2, relative_prime_number)
-    #    is_cypher = computeGCD(cypher_key, relative_prime_number)
-    #return cypher_key
-
-def calculate_decypher_key(module, relative_prime_number, cypher_key):
-
+def calculate_decypher_key(system_module, phi_number, cypher_key):
     sys.setrecursionlimit(1000000)
 
-    #(this-1 mod m).
-    #e.modInverse(euler);
-    #cypherkey elevado na menos 1 mod module
+    #calcula o numero inverso da cypher key
+    d = modinv(cypher_key, phi_number)
 
-    # d = e.modInverse(euler);
-
-    d = modinv(cypher_key, relative_prime_number)
-
-    is_decypher = (d * cypher_key) % relative_prime_number
+    #valida se D * cypher key mod phi é igual a 1, validando que é seu inverso realmente
+    is_decypher = (d * cypher_key) % phi_number
     if(is_decypher == 1):
         return d
 
-
-
-    #while not is_decypher:
-    #    decypher_key = random.randrange(0, module)
-    #    is_decypher = (cypher_key * decypher_key) % relative_prime_number  == 1
-    #return decypher_key
-
-#def computeGCD(x, y): 
-#   while(y): 
-#       x, y = y, x % y
-#   return x == 1
-
-
-def computeGCD(a, b):
-    global count
-    count = count + 1
+#algoritmo extendido de euclides fornecido
+def egcd(a, b):
     if a == 0:
         return (b, 0, 1)
     else:
-        g, y, x = computeGCD(b % a, a)
+        g, y, x = egcd(b % a, a)
         return (g, x - (b // a) * y, y)
 
+#calculo do módulo inverso
 def modinv(a, m):
-    g, x, y = computeGCD(a, m)
+    g, x, y = egcd(a, m)
     if g != 1:
         raise Exception('modular inverse does not exist')
     else:
         return x % m
 
 
-
+def hex_to_ascii(hex_str):
+    hex_str = hex_str.replace(' ', '').replace('0x', '').replace('\t', '').replace('\n', '')
+    ascii_str = binascii.unhexlify(hex_str.encode())
+    return ascii_str
 
 if __name__ == "__main__":
 
-    message = 9
-
-
-    
+    #numero P primo
     p_prime_number = generate_large_prime()
-    print("Prime number P: ", p_prime_number)
     
+    #numero Q primo
     q_prime_number = generate_large_prime()
-    print("Prime number Q: ", q_prime_number)
 
-    module = p_prime_number * q_prime_number #N
+    #módulo do sistema N
+    system_module = p_prime_number * q_prime_number
 
-    relative_prime_number = (p_prime_number - 1) * (q_prime_number - 1) #ROLA DE N
-    print("Relative Primes: ", relative_prime_number)
+    #numero phi de N
+    phi_number = (p_prime_number - 1) * (q_prime_number - 1)
 
-    cypher_key = calculate_cypher_key(relative_prime_number)#E
+    #chave para cifrar E
+    cypher_key = calculate_cypher_key(phi_number)
 
-    print ("count AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", count)
+    print ("Chave Pública: \nE > ", cypher_key, "\nN > ", system_module)
 
-    decypher_key = calculate_decypher_key(module, relative_prime_number, cypher_key)#D
+    #chave para decifrar D
+    decypher_key = calculate_decypher_key(system_module, phi_number, cypher_key)
 
-    print ("count BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB", count)
+    print ("Chave Privada: \nD > ", decypher_key, "\nP > ", p_prime_number, "\nQ > ", q_prime_number,)
 
-    print("Public Key: ", cypher_key, " and ", module)
+    menu_option = 0
+    exit_system = False
+    message = None
 
-    cypher_text = pow(message, cypher_key, module)
+    while not exit_system:
+        print("Digite um número")
+        print("1 - Cifrar Mensagem")
+        print("2 - Decifrar Mensagem")
+        menu_option = int(input("0 - Encerrar Programa\n"))
 
-    print("Cypher Text: ", cypher_text)
+        if menu_option == 1:
+            message = input("Digite a mensagem\n")
+            message_bytes = message.encode()
 
-    decypher_text = pow(cypher_text, decypher_key, module)
+            hex_message = binascii.hexlify(message_bytes)
 
-    print("Decypher Text", decypher_text)
+            int_message = int(hex_message, 16)
+            cypher_text = pow(int_message, cypher_key, system_module)
+            print("Texto Cifrado: ", cypher_text)
 
+        elif menu_option == 2:
+            message = int(input("Digite o texto cifrado\n"))
+            decypher_int = pow(message, decypher_key, system_module)
+            decypher_hex = hex(decypher_int)
 
-# 1 - calcula 2 numeros primos grandes: p e q
-# 2 - multiplica eles
-# 3 - calcula numeros que são primos relativos = (p-1) * (q-1)
-# 4 - escolher um numero maior q 1 dentro do universo de numeros acima que seja relativamente primo ao numero calculado (mdc(gcd em ingles) = 1)
-# 5 - achar o numero inverso do que tu calculou ali em cima no módulo de primos relativos
+            text = hex_to_ascii(decypher_hex)
 
-#chave publica {passso4, passo2}
-#chave privada {passo5, p do passo1, q do passo2}
+            print("Texto Decifrado: ", text.decode())
+        else:
+            exit_system = True
 
-# para cifrar faz mensagem elevado no passo4 mod passo2
-# decifrar faz mensagem cifrada elevado no passo5 mod passo2
-
-
-
-
-
-
-
-
-
-
-
-
+    sys.exit(0)
